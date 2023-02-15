b8d6cee85327499a6cd0724570a5bd0df1a56fee
STS\ngoc.vu <ngoc.vu@saigontechnology.com>
2023-02-15 11:41:27 +0700

    second

---
+++ get-link.py
@@ -0,0 +1,23 @@
+# your link goes here
+# link = "https://github.com/knightlab-analyses/qurro-mackerel-analysis/blob/master/AnalysisOutput/qurro-plot.qzv"
+link = "https://github.com/ngocvu-devops/resume-vuvanbaongoc/blob/main/picture-square.png"
+
+# note: this will break if a repo/organization or subfolder is named "blob" -- would be ideal to use a fancy regex
+# to be more precise here
+print(link.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/"))
+
+# example output link:
+# https://raw.githubusercontent.com/knightlab-analyses/qurro-mackerel-analysis/master/AnalysisOutput/qurro-plot.qzv
+
+
+
+https://raw.githubusercontent.com/nouchka/japromis.katagena.com/master/picture.jpg
+
+final
+https://raw.githubusercontent.com/ngocvu-devops/resume-vuvanbaongoc/main/picture-square.png
+
+
+
+
+
+
